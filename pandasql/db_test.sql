db120900669ec20986def6086af94156d76c6f1e
Lasha101 <gurshabo55@gmail.com>
2026-01-15 23:24:11 +0400

    add .sql file with sql queries

---
+++ pandasql/db_test.sql
@@ -0,0 +1,8 @@
+-- CREATE TABLE IF NOT EXISTS test_table (
+--     id INTEGER PIMARY KEY,
+--     info TEXT
+-- );
+
+-- INSERT INTO test_table (info) VALUES ("Hello, World!"); 
+
+SELECT * FROM test_table;
