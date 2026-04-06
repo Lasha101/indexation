2f799e1a9be3add3c31a379e7c5c9505c376e3b1
Lasha101 <gurshabo55@gmail.com>
2026-04-06 13:36:00 +0400

    Refactor database operations into modular functions and improve timer precision.
    Extract connection, cursor, query execution, fetching, and commit logic into dedicated helper functions for better modularity.
    
    Replace time.time() with time.perf_counter() in measure_exec_time for higher precision performance profiling.
    
    Update the main execution flow to utilize the newly created modular functions.

--- indexing/index.py
+++ indexing/index.py
@@ -10,38 +10,60 @@
 # cursor = db_cursor(conn)
 
 
-import sqlite3
+from sqlite3 import connect
 import time
 
 def measure_exec_time(called_func, *args, **kwargs):
-    start_time = time.time()
+    start_time = time.perf_counter()
     called_func(*args, **kwargs) 
-    return time.time() - start_time
+    return time.perf_counter() - start_time
 
-def query_executor(query, params=()):
-    cursor.execute(query, params)
+
+
+def query_exec(query, *args):
+    return cursor.execute(query, *args)
+
+def fetchall_exec(cursor):
     return cursor.fetchall()
 
-conn = sqlite3.connect(':memory:')
-cursor = conn.cursor()
+def connection():
+    return connect(':memory:')
 
-cursor.execute('''CREATE TABLE numbers (
+def crsr(conn):
+    return conn.cursor()
+
+def commiter():
+    conn.commit()
+
+
+conn = connection()
+
+cursor = crsr(conn)
+
+def query_executor(query, *args):
+    query_exec(query, *args)
+    fetchall_exec(cursor)
+
+
+
+query_exec('''CREATE TABLE numbers (
                id INTEGER PRIMARY KEY,
                value INTEGER)
 ''')
 
 num_rows = 100000
 cursor.executemany('INSERT INTO numbers (value) VALUES (?)', [(i,) for i in range(num_rows)])
-conn.commit()
+commiter()
 
 query_string = "SELECT * FROM numbers WHERE value = ?"
 query_params = (99999,)
 
-
 non_indexed_time = measure_exec_time(query_executor, query_string, query_params)
 
-cursor.execute('CREATE INDEX idx_value ON numbers(value)')
-conn.commit()
+
+
+query_exec('CREATE INDEX idx_value ON numbers(value)')
+commiter()
 
 
 indexed_time = measure_exec_time(query_executor, query_string, query_params)
